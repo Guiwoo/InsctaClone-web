0904ca0b1ae85409c4438a59ea4d5da617e81a5d
Guiwoo <park.guiwoo@hotmail.com>
2021-07-19 19:13:13 +0900

    Set up for Front
    styled componnet and  styled reset

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import { useReactiveVar } from "@apollo/client";
-import { useState } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom";
-import { isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import { GlobalStyles } from "./styles";
+
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "Lightgray",
+};
+const darkTheme = {
+  fontColor: "Lightgray",
+  bgColor: "#2c2c2c",
+};
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <Router>
-      <Switch>
-        <Route path="/" eaxt>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path="/" eaxt>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,20 @@
-import { isLoggedInVar } from "../apollo";
+import { darkModeVar, isLoggedInVar } from "../apollo";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
 
 const Login = () => (
-  <div>
-    <h1>Log-in</h1>
-    <button onClick={() => isLoggedInVar(true)}>로그인 하자</button>
-  </div>
+  <Container>
+    <Title>Log-in</Title>
+    <button onClick={() => darkModeVar(true)}>다크 모드</button>
+    <button onClick={() => darkModeVar(false)}>라이트 모드</button>
+  </Container>
 );
 
 export default Login;
